da4b4960120d23cf6cfa5a6dc23550ff85c114ed
Maya <mayaelee28@gmail.com>
2021-09-08 18:23:27 -0400

    changed buttons to new app. added label change.

--- To-do list/ViewController.swift
+++ To-do list/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Hello: UIButton!
+    
+    @IBOutlet weak var AppTitle: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func m_hello(_ sender: Any) {
+        AppTitle.text = "HIIII"
+        
+    
+    }
+    
 }
 
